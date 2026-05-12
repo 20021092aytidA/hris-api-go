ad3d539f167d0ea879867ba95b6742fb0eb7a2fd
Aditya Cahyadi Sujanadi <aditya.cahyadi.29@gmail.com>
2026-05-12 15:30:23 +0700

    add: init routes in main.go

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"go-hrs/config/database"
 	"go-hrs/config/env"
 	"go-hrs/middleware/cors"
+	"go-hrs/routes/request"
+	"go-hrs/routes/role"
+	"go-hrs/routes/user"
+	"go-hrs/routes/userdetail"
 	"os"
 	"strings"
 
@@ -22,6 +26,10 @@ func main() {
 	app.Use(cors.Setup())
 
 	// ROUTES
+	request.InitRoute(app)
+	role.InitRoute(app)
+	user.InitRoute(app)
+	userdetail.InitRoute(app)
 
 	app.Run(fmt.Sprintf("localhost:%s", os.Getenv("API_PORT")))
 }
